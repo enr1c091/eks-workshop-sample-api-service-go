f417ef0627cc6838069701d7dc8dfeaad5b47067
Enrico Bergamo <enrico_bergamo_91@hotmail.com>
2019-09-01 17:53:49 -0300

    a

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World from Enrico"}
+		res := &response{Message: "Hello World from Enrico!!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
